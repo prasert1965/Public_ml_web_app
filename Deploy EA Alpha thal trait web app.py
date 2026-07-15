5d7447a921a598b4b72e1991b38ccd1bba112806
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-15 22:09:23 +0700

    Update Deploy EA Alpha thal trait web app.py

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -12,7 +12,7 @@ import streamlit as st
 #import AdaBoostClassifier
 
 # loading the saved model
-pickle.load(open('EAtrained_model.sav', 'rb'))
+loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
 
 
 def EA_Alpha_thal_prediction(input_data):
