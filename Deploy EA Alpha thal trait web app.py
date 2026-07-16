3fe649f59531bbd1abd72ef1ef47dbcf2631bb83
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-16 09:47:34 +0700

    Update Deploy EA Alpha thal trait web app.py

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -8,7 +8,7 @@ Created on Thu Mar 21 16:20:43 2024
 import numpy as np
 import pickle
 import streamlit as st
-import scikit-learn
+#import scikit-learn
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
