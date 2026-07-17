0e4d9f12f666a60589d8c8a628a9fd645df0c09a
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-18 06:10:47 +0700

    Update model loading and fix minor formatting issues

--- Adaboost Prediction system.py
+++ Adaboost Prediction system.py
@@ -15,7 +15,7 @@ loaded_model = [LogisticRegression(), SGDClassifier()]
 
 
 # loading the saved model
-loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
+loaded_model = pickle.load(open('EAtrained_model1.sav', 'rb'))
 
 
 def EA_Alpha_thal_prediction(input_data):
