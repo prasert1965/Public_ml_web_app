bf243ddd75e3a9a9c11b6d51c4e44a62ede68a9d
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-17 17:15:55 +0700

    Update Adaboost Prediction system.py

--- Adaboost Prediction system.py
+++ Adaboost Prediction system.py
@@ -14,25 +14,18 @@ scaler = StandardScaler()
 # loading the saved model
 loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
 
-input_data = (30,44.2,14.5,5.41,81.7,26.8,32.8,14)
 
-# changing the input_data to numpy array
+def EA_Alpha_thal_prediction(input_data):
+    # changing the input_data to numpy array
+     input_data_as_numpy_array = np.asarray(input_data)
 
+    # reshape the array as we are predicting for one instance
+     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-input_data_as_numpy_array = np.asarray(input_data)
+     prediction = loaded_model.predict(input_data_reshaped)
+     print(prediction)
 
-print(input_data_as_numpy_array)
-
-# reshape the array as we are predicting for one instance
-
-input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-print(input_data_reshaped)
-
-prediction = loaded_model.predict(input_data_reshaped)
-print(prediction)
-
-if (prediction[0] == 0):
-  print('This person is Alpha Thalassemia Triat')
-else:
-  print('This person is Normal')
+     if (prediction[0] == 0):
+       return 'This person is alpha thalassemia carrier'
+     else:
+       return 'This person is not alpha thalassemia carrier'
