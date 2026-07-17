941f29a4b7adca24da58a3e700e76f7a08ed0ace
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-17 22:11:20 +0700

    Update Adaboost Prediction system.py

--- Adaboost Prediction system.py
+++ Adaboost Prediction system.py
@@ -12,7 +12,7 @@ import pickle
 #scaler = StandardScaler()
 
 # loading the saved model
-loaded_model = pickle.load(open('EAtrained_model2.sav', 'rb'))
+loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
 
 
 def EA_Alpha_thal_prediction(input_data):
