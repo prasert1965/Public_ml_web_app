240b14d5df4b423cbe94ed9d61b3271c96456148
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-18 13:12:53 +0700

    Update Deploy EA Alpha thal trait web app.py

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -19,7 +19,7 @@ from sklearn.linear_model import LogisticRegression
 loaded_model = pickle.load(open('EAtrained_model1.sav', 'rb'))
 
 
-loaded_model =  EA_Alpha_thal_prediction(input_data):
+def EA_Alpha_thal_prediction(input_data):
     
 
     # changing the input_data to numpy array
