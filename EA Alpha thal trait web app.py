b50b22a624a46f11cfb68f0c8b21ad1981165814
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-18 21:35:06 +0700

    Update EA Alpha thal trait web app.py

--- EA Alpha thal trait web app.py
+++ EA Alpha thal trait web app.py
@@ -19,8 +19,9 @@ loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
     
 
 def EA_Alpha_thal_prediction(input_data):
-    
-
+ val = "18,"
+# Strip the trailing comma
+clean_val = float(val.strip(","))  # Returns 18.0
     # changing the input_data to numpy array
      input_data_as_numpy_array = np.asarray(input_data)
 
