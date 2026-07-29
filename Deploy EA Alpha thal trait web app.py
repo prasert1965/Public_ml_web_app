19997d33a037b37271c59bfb328be1ed5418c64b
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-29 21:30:54 +0700

    Update Deploy EA Alpha thal trait web app.py

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -81,25 +81,25 @@ def create_pdf(prediction_result):
 
 # Streamlit UI
 st.title("Adaboost Model Prediction App")
-loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
+#loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
 
-def EA_Alpha_thal_prediction(input_data):
+#def EA_Alpha_thal_prediction(input_data):
   
 
     # changing the input_data to numpy array
-     input_data_as_numpy_array = np.asarray(input_data)
+ #    input_data_as_numpy_array = np.asarray(input_data)
 
     # reshape the array as we are predicting for one instance
-     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+  #   input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-     prediction = loaded_model.predict(input_data_reshaped)
-     print(input_data_as_numpy_array)
-     print(prediction)
+   #  prediction = loaded_model.predict(input_data_reshaped)
+   #  print(input_data_as_numpy_array)
+   #  print(prediction)
 
  # if prediction == 0):
      prediction = 'This person is alpha thalassemia carrier' # Example prediction
  # else:
-     predictuin = 'This person is not alpha thalassemia carrier' # Example prediction
+    # predictuin = 'This person is not alpha thalassemia carrier' # Example prediction
        
 if st.button("Generate Report"):
   pdf_data = create_pdf(prediction)
