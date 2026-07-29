b81a68f22f9fef0b0c6405f2ac59c5a008e42c64
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-29 20:11:22 +0700

    Enhance prediction app and add PDF report feature
    Updated the web app for predicting alpha thalassemia carrier status and added PDF report generation functionality.

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -81,7 +81,7 @@ def create_pdf(prediction_result):
 
 # Streamlit UI
 st.title("Adaboost Model Prediction App")
-prediction = '' # Example prediction
+prediction = 'diagnosis' # Example prediction
        
 if st.button("Generate Report"):
   pdf_data = create_pdf(prediction)
