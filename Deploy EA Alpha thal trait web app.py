52c96a0fc13d85e184d7aef2bff23eebd0669853
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-29 19:43:21 +0700

    Update Deploy EA Alpha thal trait web app.py

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -81,7 +81,7 @@ def create_pdf(prediction_result):
 
 # Streamlit UI
 st.title("Adaboost Model Prediction App")
-prediction = st.return # Example prediction
+prediction = print(prediction) # Example prediction
        
 if st.button("Generate Report"):
   pdf_data = create_pdf(prediction)
