60e9e829f580b0db44131576acbbcd300ffa96a0
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-29 19:30:21 +0700

    Refactor prediction app and add PDF report feature

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -81,7 +81,7 @@ def create_pdf(prediction_result):
 
 # Streamlit UI
 st.title("Adaboost Model Prediction App")
-   prediction = "XXXXX" # Example prediction
+  prediction = "XXXXX" # Example prediction
        
 if st.button("Generate Report"):
   pdf_data = create_pdf(prediction)
