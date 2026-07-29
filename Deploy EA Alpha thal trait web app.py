4cba5f038ca1c7a846c58b8497a60557e5797b82
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-29 21:02:35 +0700

    Update Deploy EA Alpha thal trait web app.py

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -82,7 +82,7 @@ def create_pdf(prediction_result):
 # Streamlit UI
 st.title("Adaboost Model Prediction App")
 
-     if  diagnosis = 0):
+     if  diagnosis() = 0):
       prediction = 'This person is alpha thalassemia carrier' # Example prediction
      else:
       prediction = 'This person is not alpha thalassemia carrier' # Example prediction
