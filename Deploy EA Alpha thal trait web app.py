8bd2441d1c52dec2cf548d87b0cad788d3e2c34c
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-29 21:09:46 +0700

    Refactor prediction app and add PDF report generation

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -81,8 +81,22 @@ def create_pdf(prediction_result):
 
 # Streamlit UI
 st.title("Adaboost Model Prediction App")
+loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
 
-     if  prediction == 0):
+def EA_Alpha_thal_prediction(input_data):
+  
+
+    # changing the input_data to numpy array
+     input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+     prediction = loaded_model.predict(input_data_reshaped)
+     print(input_data_as_numpy_array)
+     print(prediction)
+
+     if (prediction[0] == 0):
       prediction = 'This person is alpha thalassemia carrier' # Example prediction
      else:
       prediction = 'This person is not alpha thalassemia carrier' # Example prediction
