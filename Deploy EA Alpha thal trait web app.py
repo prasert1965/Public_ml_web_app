8d1fbb2440ab6ec8fde6e3a01b9c9bbcd49e82bb
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-06 11:04:18 +0700

    Enhance prediction result display with additional info
    Added additional output for prediction confirmation and approval.

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -65,10 +65,17 @@ def main():
     if st.button('Prediction result Pls. Click'):        
        diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
        st.success(diagnosis)
-       
+       col1, col2, col3, = st.columns(3)
+       with col1:
+          st.write('Predicted by ..Phrae ADA ML.. ') 
+       with col2:
+          st.write('Reported by ............................ ')   
+       with col3:
+          st.write('Approved by ............................ ')
+
 if __name__ == '__main__':
     main() 
-st.title("รายงานผลการทำนาย")
+#st.title("รายงานผลการทำนาย")
 # สร้างปุ่มพิมพ์หน้าเว็บ
 components.html("""
     <button onclick="window.parent.print()" style="
