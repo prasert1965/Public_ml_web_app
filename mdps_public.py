cf44b21b063ab905d061eb8f09b5800fafd7bdb6
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-12 10:02:37 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -76,7 +76,7 @@ if (selected == 'Diabetes Prediction'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
         
         if (diab_prediction[0] == 1):
-         diab_diagnosis = 'The person is diabetic'
+          diab_diagnosis = 'The person is diabetic'
         else:
            diab_diagnosis = 'The person is not diabetic'
         
@@ -144,7 +144,7 @@ if (selected == 'Heart Disease Prediction'):
         heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
         
         if (heart_prediction[0] == 1):
-         heart_diagnosis = 'The person is having heart disease'
+          heart_diagnosis = 'The person is having heart disease'
         else:
            heart_diagnosis = 'The person does not have any heart disease'
         
@@ -237,7 +237,7 @@ if (selected == "Parkinsons Prediction"):
         parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
         
         if (parkinsons_prediction[0] == 1):
-         parkinsons_diagnosis = "The person has Parkinson's disease"
+          parkinsons_diagnosis = "The person has Parkinson's disease"
         else:
            parkinsons_diagnosis = "The person does not have Parkinson's disease"
         
